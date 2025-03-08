c17a8297420a2c48f47688963cb0847a8ff90851
ki-hi-ro <hiroki.hiroki@icloud.com>
2025-03-08 15:29:28 +0900

    CORS設定

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'myapp'
+    'myapp',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'django_todo.urls'
@@ -129,3 +131,7 @@ REST_FRAMEWORK = {
     'rest_framework.renders.JSONRenderer'
   ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+  "http://localhost:3000" # Reactの場合
+]
