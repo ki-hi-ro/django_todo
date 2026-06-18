f6771d33927c5527df0a40d1f9424ba1e5be85d7
ki-hi-ro <hiroki.hiroki@icloud.com>
2026-06-19 06:14:50 +0900

    モデルに、categoryとdeadlineを追加

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
-# Create your models here.
 class Task(models.Model):
-  title = models.CharField(max_length=100)
-  description = models.TextField(blank=True)
-  completed = models.BooleanField(default=False)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
 
-  def __str__(self):
-    return self.title
+    category = models.CharField(
+        max_length=50,
+        blank=True
+    )
+
+    completed = models.BooleanField(default=False)
+
+    deadline = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
